288c9e53b868d2b56867ee30b3af35f92779f4fd
Claudio Gonzalez Hernandez <40004826+Claudio-2019@users.noreply.github.com>
2021-04-23 15:31:48 -0600

    implementacion de login con roles

---
+++ Controllers/RegisterController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WEB_API_TICKETS_SUPPORT.Models;
+using WEB_API_TICKETS_SUPPORT.Services;
+
+namespace WEB_API_TICKETS_SUPPORT.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegisterController : ControllerBase
+    {
+        private ServiceUserRegister RegisterUserService = new ServiceUserRegister();
+
+        [HttpPost]
+        public async Task<IActionResult> PostNewUser([FromBody] UserRegisterModel user)
+        {
+
+            if (user != null && user.Role.Contains("User"))
+            {
+                await RegisterUserService.CreateUserAccount(user);
+
+                return Ok("Se ha registrado el Usuario: " + user.Name);
+            }
+            else if (User != null && user.Role == "Admin")
+            {
+                UserAdminRegister AdminData = new UserAdminRegister()
+                {
+                    Name = user.Name,
+                    LastName = user.LastName,
+                    Phone = user.Phone,
+                    Email = user.Email,
+                    Pass = user.Pass,
+                    Role = user.Role
+                };
+                await RegisterUserService.CreateAdminAccount(AdminData);
+
+                return Ok("Se ha registrado el Usuario: " + AdminData.Name + "como administrador del sistema");
+            }
+            else
+            {
+                return BadRequest("Ocurrio un error al registrar el usuario");
+            }
+
+        }
+    }
+}
