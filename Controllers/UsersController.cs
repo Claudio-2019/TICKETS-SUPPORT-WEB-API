dcc1ff019b0e35cea20ebf13a2097d6269f96001
Claudio Gonzalez Hernandez <40004826+Claudio-2019@users.noreply.github.com>
2021-04-28 15:59:25 -0600

    cambios en directivas x2

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WEB_API_TICKETS_SUPPORT.Services;
+
+namespace WEB_API_TICKETS_SUPPORT.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private ServiceUserRegister ServiceUsers = new ServiceUserRegister();
+
+        [HttpGet]
+        public async Task<IActionResult> GetSystemUsers()
+        {
+            var users = await ServiceUsers.GetCurrentUsers();
+
+            return Ok(users);
+        }
+        
+
+    }
+}
